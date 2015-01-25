d2dd2796bb8f33557923e868f7e8b5bdf52a4fb3
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-24 23:05:49 -0500

    js events added to nav

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
+  $('li').hover(function() {
+    $( this ).addClass( "hoverNav" );
+  }, function() {
+    $( this ).removeClass( "hoverNav" );
+  });
+
+
   $('nav a').on('click', function(event){
     event.preventDefault();
-    $(this).closest("li").siblings().removeClass('activeNav');
+    $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest('li').addClass("activeNav");
   });
 
